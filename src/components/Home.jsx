2778c03c9290a1e8c6adf30320f6231dfdef6b50
Ankush <ankushshenoy97@gmail.com>
2023-08-27 23:04:24 +0530

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Home(){
     return(
-        <div className="home w-full h-[90vh] bg-cover bg-[url('https://images.pexels.com/photos/7234304/pexels-photo-7234304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] md:bg-[url('https://images.pexels.com/photos/436413/pexels-photo-436413.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+        <div className="main flex justify-center mt-4">
+        <div className="home border shadow-md shadow-black bg-center w-[80%] h-[80vh] bg-cover bg-[url('https://images.pexels.com/photos/7234304/pexels-photo-7234304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] md:bg-[url('https://images.pexels.com/photos/436413/pexels-photo-436413.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
             <div className="text-3xl w-full h-[70%] flex justify-center items-center text-white p-4 pt-4 md:px-8">
-                <p className='text-center font-bold italic w-full'>A captivating blend of storytelling, visuals, and emotion that transport audiences to new worlds and perspectives.
-                </p>
             </div>
+        </div>
         </div>
     )
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,8 +29,8 @@ export default function Navbar(){
                     <span className={`border border-white transition-all duration-300 ${nav?`-rotate-45 -translate-y-[0.30rem]`:`rotate-0`}`}></span>
                 </div>
                 <ul className={`navlist text-white absolute w-full bg-cyan-950 transition-all duration-300 p-6 items-center space-y-6 flex flex-col top-16 ${nav?`left-0`:`-left-full`} md:flex-row md:space-x-8 md:static md:w-fit md:p-0 md:bg-none md:z-0 md:space-y-0 md:transition-none md:duration-0`}>
-                    <NavLink to="/" onClick={()=>{setNav(!nav)}} className="w-full text-center">Home</NavLink>
-                    <NavLink to="/movies" onClick={()=>{setNav(!nav)}} className="w-full text-center">Movies</NavLink>
+                    <NavLink to="/" onClick={()=>{setNav(!nav)}} className="w-full  flex items-center justify-center"><i class='bx bx-home'></i>Home</NavLink>
+                    <NavLink to="/movies" onClick={()=>{setNav(!nav)}} className="w-full  flex items-center justify-center"><i class='bx bx-movie' ></i>Movies</NavLink>
                 </ul>
             </nav>
         </>
